6b2f6230feb71aacacac7e99a6841ec32621ad5f
Vishal <43312718+vishal0930@users.noreply.github.com>
2022-11-01 18:33:06 +0530

    Add files via upload
    Assignment 2 (JSX)

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1>The Evolution of <br/> Airbnb's Frontend</h1>
+        <p>@spikebrehm</p>
+        <h6>airbnd</h6>
+      </div>
+    </>
+  );
+}
+
+export default App;
